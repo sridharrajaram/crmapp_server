c134e7634cce023ab45e367c2b48240cfd3abe2a
Sridhar Rajaram <srilumia@hotmail.com>
2022-02-25 20:59:27 +0530

    cors updated

--- app.js
+++ app.js
@@ -24,16 +24,12 @@ mongoose.connect(
   }
 );
 
-const corsOptions = {
-  origin: "*"
-};
-
 // configure express
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(helmet());
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
